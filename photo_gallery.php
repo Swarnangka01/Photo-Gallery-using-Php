fdf336916c6742479c42213ce58b96838025b52b
Swarnangka01 <127179707+Swarnangka01@users.noreply.github.com>
2023-08-29 12:47:09 +0530

    Add files via upload

---
+++ photo_gallery.php
@@ -0,0 +1,74 @@
+<html>
+    <head>
+        <title></title>
+		<link rel="stylesheet" href="photo_gallery.css">    
+	</head>
+    <style>
+		.button{
+			text-decoration:none;
+			color:white;
+		}
+	
+    </style>
+    <body>
+    <header><?php  include('new_header.php'); ?></header>
+	
+	<!-- <div class="container" style="background-color:lightgray;margin-bottom:10px;width:100%;margin-top:-0.5%;">
+            <div class="downdiv">
+                <div class="updateword">
+				<a class="link" href="" style="color:white;">BACK /</a> PHOTO GALLERY :
+                </div>
+            </div>
+			</div>  -->
+	<!-- <div class="heading_container">
+    <div class="back_button">HOME</div>
+    <div class="photo_gal">PHOTO GALLERY</div>
+    <div class="home_button">BACK</div>
+  </div> -->
+	<main>
+ <!-- Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui laudantium repudiandae eligendi, corporis, hic, dolores labore ab inventore ea beatae temporibus quae aspernatur cum molestiae tempore quidem dolorum odio magnam autem voluptatibus modi! Sunt sapiente repellat natus quidem quisquam. Ipsum, adipisci, deserunt iure eum repudiandae suscipit sint optio laborum, beatae doloremque alias porro quam ratione praesentium quod assumenda! Ea, dignissimos! Praesentium cumque placeat facilis architecto error assumenda, ab accusamus porro enim est impedit sed vero totam nostrum, dolor excepturi dignissimos magni perspiciatis, et ad. Expedita minus similique atque commodi, quae odit corporis maiores doloremque provident aut quas tempore fugiat nihil vitae veritatis. Laboriosam, sed fuga reprehenderit enim architecto quam nemo a maxime recusandae libero obcaecati placeat aspernatur reiciendis saepe explicabo, dolor nam velit magnam omnis aliquam quo at quia. Facere veniam neque iusto dolores. Odio est soluta laborum? Quia sequi voluptas tempore possimus rerum quae ad sed alias, reiciendis provident delectus asperiores voluptates est velit aspernatur iste, dolores iure dolor atque, omnis veritatis consequatur! Praesentium dolore nostrum maiores nemo tempore obcaecati esse culpa repudiandae voluptatem exercitationem minima, vero, ad eius sit rerum rem ullam commodi est eveniet minus reprehenderit numquam nihil expedita? Minima illo accusamus sunt sed amet molestias odio architecto debitis iste nisi inventore voluptates esse perferendis a nostrum ducimus animi ipsum, doloremque reiciendis consectetur dolore dolorum? Et omnis voluptate expedita, porro atque, sed vitae doloribus laudantium explicabo doloremque nulla, magnam autem corrupti! Blanditiis nisi, sint soluta suscipit sunt vero maxime! Blanditiis beatae delectus cumque ullam porro voluptate sunt consectetur error quis nobis reprehenderit placeat, ea fugiat doloremque, optio quia modi. Expedita magni provident corporis nostrum doloribus libero incidunt distinctio nulla voluptatibus atque magnam quaerat sequi alias, cumque obcaecati beatae nobis recusandae quis, harum vitae animi rem laudantium dolore optio? Ipsa voluptatem temporibus tempore corrupti veritatis optio. Laborum debitis similique quos explicabo libero consequuntur dolore quaerat cum. Velit beatae quia, quo alias ipsum, cum, dignissimos unde molestias eum nostrum inventore! Esse hic alias laborum minus odio eaque excepturi doloremque, quia aliquid quam, ab fugiat. Ex quia alias tenetur consectetur nesciunt soluta ullam dicta laudantium, nemo voluptate, natus aliquid quibusdam sunt recusandae excepturi, asperiores autem ad nostrum tempore. Ea obcaecati eveniet nisi quo, rem non, aut quaerat officiis sequi, asperiores placeat consectetur reiciendis optio odit? Dolor, dolorum adipisci reprehenderit similique ratione dolores facilis delectus labore optio facere sequi veniam officiis officia aperiam eligendi explicabo aliquam tempora repellendus cumque voluptate rerum fugiat perferendis! Obcaecati autem aut ut temporibus perspiciatis veritatis nam, ab dicta veniam? Ab officiis id quidem, dolor perferendis ducimus doloremque, eius dolore praesentium laborum sunt, totam neque atque tenetur? Est possimus consequuntur dolore reiciendis, vero obcaecati recusandae harum placeat itaque deleniti labore libero eum facilis at ea, repudiandae omnis perferendis commodi asperiores laudantium repellendus illum tenetur quibusdam reprehenderit. Dolorem sint quas similique qui dicta perferendis neque molestiae consequatur optio molestias assumenda commodi ipsam amet voluptatem accusantium omnis deleniti et, impedit delectus eum officia non natus pariatur? Error repellat delectus quisquam modi ratione, dolore quasi reprehenderit accusamus vitae cumque reiciendis non corrupti voluptatibus corporis iure commodi! Quis facere eum repudiandae commodi, molestiae iure aspernatur veniam sequi! Corrupti sit dolores possimus eaque itaque unde obcaecati natus laudantium voluptas dicta. Voluptatum fuga voluptas optio velit libero quaerat eius quam animi. Amet eveniet maiores mollitia eum, a eius voluptatem officiis provident dolores, ipsum, ducimus minus. Temporibus eum, dolores quas debitis ullam qui obcaecati harum fuga provident voluptas non dolor modi ratione labore eveniet eius culpa possimus omnis vel exercitationem dicta voluptates? Nostrum dignissimos vel officia porro, placeat sequi ut neque ad asperiores recusandae aut earum ab voluptates culpa, expedita, odit mollitia quas nihil.
+<div class="gallery" style="background-color:lightgray;" >-->
+	<!-- <div class="gallery">		 -->
+	<!-- <div class="gal_header">	 -->
+	
+
+		<div class="sports" >
+		<a href="sports.php" style="text-decoration:none;">
+		<img src="inside/sports/sports_1.jpg" width="100%" height="100%"/><br>
+		<span style="height:5px;position:relative; padding:2px 3px 2px 3px;; border-radius:5px; color:#9c1f2e;text-decoration:none;">Sports</span>
+		</a>
+		</div>
+
+		<div class="sports">
+		<a href="cars.php" style="text-decoration:none;">
+		<img src="inside/cars/cars_1.jpg" width="100%" height="100%"/><br>
+		<span style="height:5px;position:relative; padding:2px 3px 2px 3px; border-radius:5px; color:#9c1f2e;">Cars</span>
+		</a>
+		</div>
+
+		<div class="sports">
+		<a href="watches.php" style="text-decoration:none;">
+		<img src="inside/watches/watch_1.jpg" width="100%" height="100%"/><br>
+		<span style="height:5px;position:relative; padding:2px 3px 2px 3px; border-radius:5px; color:#9c1f2e;">Watches</span>
+		</a>
+		</div>
+		
+		<div class="sports">
+		<a href="landscape.php" style="text-decoration:none;">
+		<img src="inside/landscape/landscape_1.jpg" width="100%" height="100%"/><br>
+		<span style="position:relative; padding:2px 3px 2px 3px; border-radius:5px; color:#9c1f2e;">Landscape</span>
+		</a>
+		</div>
+
+		<div class="sports">
+		<a href="sketches.php" style="text-decoration:none;">
+		<img src="inside/sketches/Simple-Pencil-Sketches-Of-Couples-In-Love-1.jpg" width="100%" height="100%"/><br>
+		<span style="position:relative; padding:2px 3px 2px 3px; border-radius:5px; color:#9c1f2e;">Sketches</span>
+		</a>
+		</div>
+
+		
+</main>
+<footer><?php  include('new_footer.php'); ?></footer>
+   </body>
+</html>
