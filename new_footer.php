ff9c25a708ae7f5ea51ae82c33519f173371c3f4
Swarnangka01 <127179707+Swarnangka01@users.noreply.github.com>
2023-11-05 00:58:38 +0530

    Add files via upload

---
+++ new_footer.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <style>
+        body{
+            margin:0%;
+        }
+        .footer{
+            background-color:transparent;
+            text-align:center;
+            width:100%;
+            height:10%;
+        }
+        .copyright{
+            display:flex;
+            flex-direction:row;
+            justify-content: space-between;
+        }
+    </style>
+    <body>
+        <!-- <div class="footer"> -->
+            <div class="footer">
+            <!-- <div class="copyright"> -->
+                <div >&copy; 2023 Photo Gallery using Php. All rights reserved.</div>
+                
+            <!-- </div> -->
+            </div>
+    <!-- </div> -->
+    </body>
+</html>
---
+++ photo_gallery.php
@@ -0,0 +1,56 @@
+<html>
+    <head>
+        <title></title>
+		<link rel="stylesheet" href="photo_gallery.css">    
+	</head>
+    <style>
+		.button{
+			text-decoration:none;
+			color:white;
+		}
+	
+    </style>
+    <body>
+    <header><?php  include('new_header.php'); ?></header>
+	
+	<main>
+		<div class="sports" >
+		<a href="sports.php" style="text-decoration:none;">
+		<img src="inside/sports/sport4.jpg" width="100%" height="100%"/><br>
+		<span style="height:5px;position:relative; padding:2px 3px 2px 3px;; border-radius:5px; color:#9c1f2e;text-decoration:none;">Sports</span>
+		</a>
+		</div>
+
+		<div class="sports">
+		<a href="cars.php" style="text-decoration:none;">
+		<img src="inside/cars/car1.jpg" width="100%" height="100%"/><br>
+		<span style="height:5px;position:relative; padding:2px 3px 2px 3px; border-radius:5px; color:#9c1f2e;">Cars</span>
+		</a>
+		</div>
+
+		<div class="sports">
+		<a href="watches.php" style="text-decoration:none;">
+		<img src="inside/watches/watch2.jpeg" width="100%" height="100%"/><br>
+		<span style="height:5px;position:relative; padding:2px 3px 2px 3px; border-radius:5px; color:#9c1f2e;">Watches</span>
+		</a>
+		</div>
+		
+		<div class="sports">
+		<a href="landscape.php" style="text-decoration:none;">
+		<img src="inside/landscape/land1.jpg" width="100%" height="100%"/><br>
+		<span style="position:relative; padding:2px 3px 2px 3px; border-radius:5px; color:#9c1f2e;">Landscape</span>
+		</a>
+		</div>
+
+		<div class="sports">
+		<a href="sketches.php" style="text-decoration:none;">
+		<img src="inside/sketches/sketch5.jpg" width="100%" height="100%"/><br>
+		<span style="position:relative; padding:2px 3px 2px 3px; border-radius:5px; color:#9c1f2e;">Sketches</span>
+		</a>
+		</div>
+
+		
+</main>
+<footer><?php  include('new_footer.php'); ?></footer>
+   </body>
+</html>
